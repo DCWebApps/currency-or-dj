c98760b003cd93b2d85f559e3d2420bfbcac8e0c
Dan Grover <dan@dangrover.com>
2018-01-01 19:48:09 -0800

    switch to grunt for builds

---
+++ Gruntfile.js
@@ -0,0 +1,94 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      concat:{
+          vendor:{
+              src:['src/js/vendor/*.js'],
+              dest:'build/js/vendor.js'
+          },
+          app:{
+              src:['src/js/*.js'],
+              dest:'build/js/app.js'
+          }
+      },
+      less:{
+          options:{
+
+          },
+          files:{
+             'build/css/main.css':'/src/less/main.less'
+          }
+      },
+      uglify:{
+          js:{
+              files:{
+                  'build/js/vendor.js':'build/js/vendor.js',
+                  'build/js/app.js':'build/js/app.js',
+              }
+          }
+      },
+      copy:{
+          html:{
+            'cwd':'src/html',
+            'src':'*.html',
+            'dest':'build',
+            'expand':true
+            },
+            data:{
+            'cwd':'src/data',
+            'src':'**',
+            'dest':'build/data',
+            'expand':true
+            },
+      },
+      watch: {
+            scripts: {
+              files: ['src/**'],
+              tasks: ['default'],
+              options: {spawn: false}
+            }
+      },
+    clean: ['build/**'],
+    'gh-pages': {
+      options: {
+        base: 'build'
+      },
+      src: ['**']
+    },
+    connect: {
+      server:{
+      options:{
+        port: 8080,
+        base: './build'
+      }
+    }
+    },
+    'json-minify': {
+    build: {
+      files: 'build/**/*.json'
+    }
+  }
+  
+   });
+  
+  
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-json-minify');
+    grunt.loadNpmTasks('grunt-gh-pages');
+
+    grunt.registerTask('default', ['concat', 'copy', 'less']);
+
+    grunt.registerTask('dev', ['default','connect', 'watch']);
+
+    grunt.registerTask('prod', ['clean','default','uglify', 'json-minify']);
+
+    grunt.registerTask('publish', ['prod', 'gh-pages']);
+  };
+  
